a9eba85c6bc708957fd0e837f74264a71ccff70d
kratikesh18 <im.kartikesh18@gmail.com>
2024-11-07 12:30:17 +0530

    Undergo Maintainenece

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Register from "./components/Register";
 
 function App() {
-  return <Register />;
+  return (
+    <div className="flex justify-center items-center bg-black w-full h-screen text-white flex-col gap-4">
+      <h1 className="text-4xl font-bold ">Heisenburg is under construction</h1>
+      <p>Soon will be available at your service</p>
+      <a href="https://github.com/kratikesh18/project-3">Code</a>
+    </div>
+  );
 }
 
 export default App;
